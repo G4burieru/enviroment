55dc7951691ca7511f59b39c6ae1bf1075c18aee
João Gabriel Santos <103967434+G4burieru@users.noreply.github.com>
2024-08-01 18:04:52 -0300

    Add files via upload

---
+++ nvim/lua/plugins/which-key.lua
@@ -0,0 +1,23 @@
+return {
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+        keys = {
+                { "<leader>f", group = "file" }, -- group
+                { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File", mode = "n" },
+                { "<leader>fb", function() print("hello") end, desc = "Foobar" },
+                { "<leader>fn", desc = "New File" },
+                { "<leader>f1", hidden = true }, -- hide this keymap
+                { "<leader>w", proxy = "<c-w>", group = "windows" }, -- proxy to window mappings
+                { "<leader>b", group = "buffers", expand = function()
+                    return require("which-key.extras").expand.buf()
+                    end
+                },
+        },
+    }
+}
