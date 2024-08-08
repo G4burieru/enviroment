925291f052737710e0c71f85fc981f1f5498afa8
G4burieru <joao.gabriel.silva.santos@academico.ufpb.br>
2024-08-08 11:55:44 -0300

    added window splitting to the keybinds

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -8,15 +8,30 @@ vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", { d
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Copy to clipboard" })
 vim.keymap.set("n", "<leader>-", require("oil").toggle_float, { desc = "Toggle Oil" })
 vim.keymap.set("n", "<leader><leader>", require("telescope").extensions.recent_files.pick, { desc = "Recent Files" })
-vim.keymap.set("n", "<leader>fa", function() builtin.find_files({cwd = '~/'}) end, { desc = "Find Files in Root" })
+vim.keymap.set("n", "<leader>fa", function()
+  builtin.find_files({ cwd = "~/" })
+end, { desc = "Find Files in Root" })
 
-
+vim.keymap.set("n", "<leader>ss", ":split<CR>", { desc = "Horizontal Split" })
+vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", { desc = "Vertical Split" })
+vim.keymap.set("n", "<leader>sh", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<leader>sj", "<C-w>j", { desc = "Move to bottom window" })
+vim.keymap.set("n", "<leader>sk", "<C-w>k", { desc = "Move to top window" })
+vim.keymap.set("n", "<leader>sl", "<C-w>l", { desc = "Move to right window" })
+vim.keymap.set("n", "<leader>s=", "<C-w>=", { desc = "Equalize window sizes" })
+vim.keymap.set("n", "<leader>s+", ":resize +2<CR>", { desc = "Increase window height" })
+vim.keymap.set("n", "<leader>s-", ":resize -2<CR>", { desc = "Decrease window height" })
+vim.keymap.set("n", "<leader>s>", ":vertical resize +2<CR>", { desc = "Increase window width" })
+vim.keymap.set("n", "<leader>s<", ":vertical resize -2<CR>", { desc = "Decrease window width" })
+vim.keymap.set("n", "<leader>sc", ":close<CR>", { desc = "Close window" })
+vim.keymap.set("n", "<leader>so", "<C-w>o", { desc = "Close other windows" })
 
 local wk = require("which-key")
 wk.add({
   { "<leader>f",  group = "file" }, -- group
   { "<leader>c",  group = "code" },
   { "<leader>g",  group = "git" },
+  { "<leader>s",  group = "window" },
   { "<leader>a",  group = "Auto-Session" },
   { "<leader>fn", desc = "New File" },
   { "<leader>w",  proxy = "<c-w>",       group = "windows" }, -- proxy to window mappings
