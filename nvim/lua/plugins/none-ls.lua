393e719761330785b6a19609d90dc12555b72d7d
G4burieru <joao.gabriel.silva.santos@academico.ufpb.br>
2024-08-08 14:40:27 -0300

    fixed formatters on null-ls

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,7 +1,21 @@
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = {
+    "jay-babu/mason-null-ls.nvim",
+  },
   config = function()
     local null_ls = require("null-ls")
+    require("mason-null-ls").setup({
+      ensure_installed = {
+        "black",
+        "isort",
+        "stylua",
+        "eslint",
+        "clang-format",
+        -- add more formatters and linters here as needed
+      },
+    })
+
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
